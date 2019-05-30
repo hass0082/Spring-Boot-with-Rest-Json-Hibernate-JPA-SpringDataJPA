1f81b6afb9c7c9d5524adfa12ce7516fbc54501a
soumyadip007 <soumyadip.note@gmail.com>
2019-05-30 18:08:43 +0530

    TestRestController class added

---
+++ Spring Boot/src/main/java/com/spring/boot/rest/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.spring.boot.rest.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestController {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestController.class, args);
+	}
+
+}
