a5941d65c6f5c8842fd4e54ca999fac3da8cf345
soumyadip007 <soumyadip.note@gmail.com>
2019-05-30 18:14:02 +0530

    Get Mapping

--- Spring Boot/src/main/java/com/spring/boot/rest/controller/TestRestController.java
+++ Spring Boot/src/main/java/com/spring/boot/rest/controller/TestRestController.java
@@ -1,10 +1,15 @@
 package com.spring.boot.rest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestRestController {
 
 	
-	
+	@GetMapping("/")
+	public String test()
+	{
+		return "Hello";
+	}
 }
