8ed3ea7e8130ee438e3c6210e8a863253d5115a5
soumyadip007 <soumyadip.note@gmail.com>
2019-05-30 18:09:57 +0530

    Test Method added

--- Spring Boot/src/main/java/com/spring/boot/Spring/Boot/Application.java
+++ Spring Boot/src/main/java/com/spring/boot/Spring/Boot/Application.java
@@ -1,13 +1,13 @@
 package com.spring.boot.Spring.Boot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	
+	@GetMapping("/")
+	public String sayHello()
+	{
+		return "HELLO";
 	}
-
 }
