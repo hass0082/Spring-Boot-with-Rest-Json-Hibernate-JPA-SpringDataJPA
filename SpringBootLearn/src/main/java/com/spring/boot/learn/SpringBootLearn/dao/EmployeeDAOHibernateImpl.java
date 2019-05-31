3eba951c728912f280881f83df26063d4f851774
soumyadip007 <soumyadip.note@gmail.com>
2019-05-31 23:42:55 +0530

    JPA Hibernate Code added

--- SpringBootLearn/src/main/java/com/spring/boot/learn/SpringBootLearn/dao/EmployeeDAOHibernateImpl.java
+++ SpringBootLearn/src/main/java/com/spring/boot/learn/SpringBootLearn/dao/EmployeeDAOHibernateImpl.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.spring.boot.learn.SpringBootLearn.entity.Employee;
 
 public class EmployeeDAOHibernateImpl implements EmployeeDAO {
@@ -22,9 +27,22 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 	
 	
 	@Override
+	@Transactional
 	public List<Employee> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	
+		//get the current hibernate session
+		Session currentSession=obj.unwrap(Session.class);
+		
+		//Create a query
+		Query <Employee> theQuery=
+					currentSession.createQuery("from Employee", Employee.class);
+		
+		//execute
+		List<Employee> employees=theQuery.getResultList();
+		
+		//return the results
+		
+		return employees;
 	}
 
 }
