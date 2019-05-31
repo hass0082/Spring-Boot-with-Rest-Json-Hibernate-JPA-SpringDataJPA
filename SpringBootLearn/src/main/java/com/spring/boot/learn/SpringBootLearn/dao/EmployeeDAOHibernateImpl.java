21f856a843de924620327c12122feb02cdf5d01a
soumyadip007 <soumyadip.note@gmail.com>
2019-05-31 23:38:29 +0530

    Constructor injection added

--- SpringBootLearn/src/main/java/com/spring/boot/learn/SpringBootLearn/dao/EmployeeDAOHibernateImpl.java
+++ SpringBootLearn/src/main/java/com/spring/boot/learn/SpringBootLearn/dao/EmployeeDAOHibernateImpl.java
@@ -2,10 +2,25 @@ package com.spring.boot.learn.SpringBootLearn.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
 import com.spring.boot.learn.SpringBootLearn.entity.Employee;
 
 public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 
+	//Define field for EntityManager  //Replacement of sessionFactory
+	
+	private EntityManager obj;
+	
+	
+	@Autowired
+	public EmployeeDAOHibernateImpl(EntityManager theEntity)
+	{
+		obj=theEntity;
+	}
+	
+	
+	
 	@Override
 	public List<Employee> findAll() {
 		// TODO Auto-generated method stub
