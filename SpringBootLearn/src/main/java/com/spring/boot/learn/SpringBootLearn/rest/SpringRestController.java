0698f1b02fea5b0cca929a78fa71d3e732d19c4f
soumyadip007 <soumyadip.note@gmail.com>
2019-05-31 22:39:59 +0530

    Property injected Coach

--- SpringBootLearn/src/main/java/com/spring/boot/learn/SpringBootLearn/rest/SpringRestController.java
+++ SpringBootLearn/src/main/java/com/spring/boot/learn/SpringBootLearn/rest/SpringRestController.java
@@ -1,11 +1,20 @@
 package com.spring.boot.learn.SpringBootLearn.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringRestController {
 
+	
+	//Injecting properties 
+	
+	@Value("${coach.name}")
+	private String coach;
+	
+	
+	
 	@GetMapping("/map")
 	public String index()
 	{
