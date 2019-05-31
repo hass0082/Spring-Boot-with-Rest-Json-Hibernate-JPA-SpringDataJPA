f8a6649d2d98037ccf44c8ecba6c2070ea6e9bb2
soumyadip007 <soumyadip.note@gmail.com>
2019-06-01 02:02:19 +0530

    Delete method added for rest

--- SpringBootLearn/src/main/java/com/spring/boot/learn/SpringBootLearn/rest/SpringRestController.java
+++ SpringBootLearn/src/main/java/com/spring/boot/learn/SpringBootLearn/rest/SpringRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,7 +75,23 @@ public class SpringRestController {
 		}
 		
 
-	
+		// add mapping for DELETE /employees/{employeeId} - delete employee
+		
+		@DeleteMapping("/employees/{employeeId}")
+		public String deleteEmployee(@PathVariable int employeeId) {
+			
+			Employee tempEmployee = employeeService.findById(employeeId);
+			
+			// throw exception if null
+			
+			if (tempEmployee == null) {
+				throw new RuntimeException("Employee id not found - " + employeeId);
+			}
+			
+			employeeService.deleteById(employeeId);
+			
+			return "Deleted employee id - " + employeeId;
+		}
 	
 	//Injecting properties 
 	
