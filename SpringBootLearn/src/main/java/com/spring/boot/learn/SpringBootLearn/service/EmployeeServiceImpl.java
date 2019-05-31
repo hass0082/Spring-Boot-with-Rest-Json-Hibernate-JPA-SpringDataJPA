11b63961f5aa126c86cb6beb8f12b16c672fe293
soumyadip007 <soumyadip.note@gmail.com>
2019-06-01 01:44:00 +0530

    Service Class added

---
+++ SpringBootLearn/src/main/java/com/spring/boot/learn/SpringBootLearn/service/EmployeeServiceImpl.java
@@ -0,0 +1,38 @@
+package com.spring.boot.learn.SpringBootLearn.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.spring.boot.learn.SpringBootLearn.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	
+	
+	@Override
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
